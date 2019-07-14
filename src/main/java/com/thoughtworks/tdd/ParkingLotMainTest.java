aadeff740b988c0f99c084c07e56cb9f40294edd
unknown <11059964896@qq.com>
2019-07-14 20:37:13 +0800

    [GRD] cover Story1-AC4

--- src/main/java/com/thoughtworks/tdd/ParkingLotMainTest.java
+++ src/main/java/com/thoughtworks/tdd/ParkingLotMainTest.java
@@ -26,17 +26,17 @@ public class ParkingLotMainTest {
         Car benz = new Car("benz","no2");
         Car honda = new Car("honda","no3");
         ParkingBoy parkingBoy = new ParkingBoy();
-        Ticket ticket = parkingBoy.parkingCar(parkingLot, car);
-        Ticket ticket1 = parkingBoy.parkingCar(parkingLot, benz);
-        Ticket ticket2 = parkingBoy.parkingCar(parkingLot, honda);
+        Ticket ticket1 = parkingBoy.parkingCar(parkingLot, car);
+        Ticket ticket2 = parkingBoy.parkingCar(parkingLot, benz);
+        Ticket ticket3 = parkingBoy.parkingCar(parkingLot, honda);
         //when
-        Car fetchCar = parkingBoy.fetchCar(parkingLot, ticket);
         Car fetchCar1 = parkingBoy.fetchCar(parkingLot, ticket1);
         Car fetchCar2 = parkingBoy.fetchCar(parkingLot, ticket2);
+        Car fetchCar3 = parkingBoy.fetchCar(parkingLot, ticket3);
         //then
-        Assertions.assertSame(car, fetchCar);
-        Assertions.assertSame(benz, fetchCar1);
-        Assertions.assertSame(honda, fetchCar2);
+        Assertions.assertSame(car, fetchCar1);
+        Assertions.assertSame(benz, fetchCar2);
+        Assertions.assertSame(honda, fetchCar3);
     }
 
     @Test
@@ -49,5 +49,19 @@ public class ParkingLotMainTest {
         Car fetchCar = parkingBoy.fetchCar(parkingLot, fakeTicket);
         //then
         Assertions.assertSame(null, fetchCar);
+    }
+
+    @Test
+    public void should_return_no_car_when_fetch_car_given_tickets_used() {
+        //given
+        Car benz = new Car("benz","no1");
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Ticket ticket = parkingBoy.parkingCar(parkingLot, benz);
+        //when
+        Car fetchCar1 = parkingBoy.fetchCar(parkingLot, ticket);
+        Car fetchCar2 = parkingBoy.fetchCar(parkingLot, ticket);
+        //then
+        Assertions.assertSame(benz, fetchCar1);
+        Assertions.assertSame(null, fetchCar2);
     }
 }
